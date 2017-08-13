c5b1ff4a0949f537d8b9ae393c97dbe958729764
Nyonya Soekarni <alba.cierr4@gmail.com>
2017-08-13 20:13:33 +0700

    oke crot

--- index.php
+++ index.php
@@ -1,8 +1,1 @@
-<div class="form-group">
-  <label for=""></label>
-  <input type="text" class="form-control" id="" placeholder="">
-  <p class="help-block">Help text here.</p>
-</div><?php
-echo 'run as hell';
-
-echo 'oke bos'
+semvak
