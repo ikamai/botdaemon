0b255193740f029a3dbccc1dc3a4a289f01bcb41
Nyonya Soekarni <alba.cierr4@gmail.com>
2017-08-13 19:56:12 +0700

    oke

--- index.php
+++ index.php
@@ -1,10 +1,2 @@
 <?php
-
-$tx = shell_exec('ps aux | grep apibot.sh');
-$tx = explode("\n",$tx);
-foreach($tx as $t){
-	if(strpos($t,'container/apibot')!==false){
-		$t = preg_replace('/\s+/', ' ', $t);
-		echo $t."\n";
-	}
-}
+echo 'run as hell';
